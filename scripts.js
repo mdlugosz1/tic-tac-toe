d913f6c36ed6a3e5b342d39d185f203a1b620513
Michał Długosz <mdugosz@gmail.com>
2021-02-25 14:11:38 +0100

    Added factory function for creating players. Player now has basic ability to mark clicked cells

--- scripts.js
+++ scripts.js
@@ -1,26 +1,55 @@
-const gameBoard = (() => {
-    const gameBoard = document.querySelector('.gameboard');
-    let board = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+const Player = (name, mark) => {
+    const getName = () => name;
+    const getMark = () => mark;
 
-    const _createBoard = () => {
-        for (let i = 0; i < 9; i++) {
-            const cell = document.createElement('div');
-            gameBoard.appendChild(cell);
-        }
-    }
+    const markCell = () => {
+        return getMark();
+    };
 
-    _createBoard();
-
-    const fillBoard = () => {
-        for (let i = 0; i < board.length; i++) {
-            const fillCells = gameBoard.querySelectorAll('div');
-            fillCells[i].textContent = board[i];
-        }
-    }
-
-    //fillBoard();
+    const playerWins = () => {
+        alert(`${getName()} wins!`);
+    };
 
     return {
-        fill : fillBoard
+        markCell,
+        playerWins
     }
+};
+
+
+const gameBoard = (() => {
+    const gameBoard = document.querySelector('.gameboard');
+    let board = ['O', '', '', '', '', '', '', '', ''];
+    const cells = gameBoard.querySelectorAll('div');
+    const playerOne = Player('Michał', 'X');
+    
+    const renederBoard = () => {
+        cells.forEach(cell => {
+            cell.addEventListener('click', () => {
+                fillBoard(cell);
+                cell.textContent = board[cell.getAttribute('id')];
+            })
+        });
+    };
+    
+    const fillBoard = (cell) => {
+        for (let i = 0; i < board.length; i++) {
+            if (Number(cell.getAttribute('id')) === i) {
+                board[i] = playerOne.markCell();
+                console.log(board);
+            }
+            
+        }
+    }
+    
+    renederBoard();
+
+    return {
+        renederBoard
+    }
+})();
+
+const gameState = (() => {
+    
+    //const playerTwo = Player('AI', 'O');
 })();
